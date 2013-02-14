9d4d1292eb29544a74a4199c042565dfaa0e0fcc
Mike <michael.prentice@tufts.edu>
2013-02-14 15:47:12 -0500

    Make install_yum_packages.bash chatty

--- install/scripts/install_from_github.bash
+++ install/scripts/install_from_github.bash
@@ -3,38 +3,61 @@
 # Install OpenTUSK from Github repository. Very much a work in
 # progress! Run as root.
 
+# Run with:
+# curl -O https://raw.github.com/mprentice/Opentusk/installer6/install/scripts/install_from_github.bash
+# bash install_from_github.bash
+
 mycwd="$PWD"
 
 myrepo="https://github.com/mprentice/Opentusk.git"
 mybranch="installer6"
 
-echo "Installing OpenTUSK and EPEL repos ..."
+echo
+echo "##########"
+echo "# Installing OpenTUSK and EPEL repos ..."
+echo "##########"
 
 cd /etc/yum.repos.d
 curl -O https://raw.github.com/mprentice/Opentusk/$mybranch/install/opentusk.repo
 yum install --quiet -y epel-release
 
-echo "Installing git ..."
+echo
+echo "##########"
+echo "# Installing git ..."
+echo "##########"
 
 yum install --quiet -y git
 
-echo "Getting OpenTUSK branch $mybranch from $myrepo ..."
+echo
+echo "##########"
+echo "# Getting OpenTUSK branch $mybranch from $myrepo ..."
+echo "##########"
 
-mkdir -p /usr/local/tusk
+mkdir --parents /usr/local/tusk
 cd /usr/local/tusk
-if [ -d Opentusk ] ; then
-    mytmp=`mktemp -d`
-    echo "Moving old Opentusk clone out of the way to $mytmp ..."
+if [ ! -d Opentusk ] ; then
+    git clone --branch $mybranch $myrepo
+else
+    echo
+    echo "##########"
+    echo "# Existing Opentusk found, skipped"
+    echo "# (You can update later with \`git pull' if needed)"
+    echo "##########"
     mv Opentusk "$mytmp"
 fi
-git clone --branch $mybranch $myrepo
 
-echo "Installing opentusk.repo ..."
+echo
+echo "##########"
+echo "# Installing opentusk.repo ..."
+echo "##########"
 
 cd Opentusk/install
 cp opentusk.repo /etc/yum.repos.d/opentusk.repo
 
-echo "Installing yum packages ..."
+echo
+echo "##########"
+echo "# Installing yum packages ..."
+echo "##########"
 
 cd scripts
 ./install_yum_packages.bash
