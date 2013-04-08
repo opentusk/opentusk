89ccae40ccce6de64807f3f716eb24e4984d90cb
Mike <michael.prentice@tufts.edu>
2013-04-08 15:36:00 -0400

    Add db version info script and lib
    Handle unversioned databases

---
+++ db/version.pl
@@ -0,0 +1,47 @@
+#! /usr/bin/env perl
+
+use Modern::Perl;
+use Getopt::Long;
+use Readonly;
+use DBI;
+use Carp;
+use TUSK::DB::Version;
+
+Readonly my $help_text => <<END_HELP;
+Usage: perl version.pl
+
+Print out the version information for each tusk database.
+
+The database connection information is read from tusk.conf. tusk.conf
+must be properly configured before this script is run.
+
+Example:
+  perl version.pl
+
+Options:
+  --help            Print usage and exit
+END_HELP
+
+my (
+    $show_help,
+);
+
+GetOptions(
+    'help' => \$show_help,
+);
+
+if ($show_help) {
+    print $help_text;
+    exit;
+}
+
+my $db_version = TUSK::DB::Version->new();
+
+my $version_string_ref = $db_version->version_string_hashref();
+foreach my $db (keys %{$version_string_ref}) {
+    my $ver = $version_string_ref->{$db};
+    print "$db version: ";
+    print $ver;
+    print ' (run baseline.pl to setup versioning)' if $ver eq 'unversioned';
+    print "\n";
+}
