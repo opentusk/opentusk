4abb9eda2432117a78dec8336cf0b96e8d0c452e
Mike Prentice <michael.prentice@tufts.edu>
2013-04-30 14:38:48 -0400

    Fix Vagrant CentOS networking (in progress)

--- Vagrantfile
+++ Vagrantfile
@@ -49,7 +49,7 @@ Vagrant.configure("2") do |config|
   
     # Use VBoxManage to customize the VM. For example to change memory:
     vb.customize ["modifyvm", :id, "--memory", "1024"]
-    vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+    vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
   end
   #
   # View the documentation for the provider you're using for more
--- install/centos-5.8/bootstrap.sh
+++ install/centos-5.8/bootstrap.sh
@@ -2,18 +2,26 @@
 
 # Installing OpenTUSK and EPEL repos
 
-echo "Configuring from Vagrant bootstrap ..."
+sn=`basename $0`
+
+echo "[$sn] Configuring from Vagrant bootstrap ..."
 
 if [ -d /vagrant ] ; then
-    echo "Vagrant detected, removing kudzu ..."
-    yum --quiet -y remove kudzu
+    sed -i 's/^nameserver\s*[0-9.]\+$/nameserver 8.8.8.8/g' /etc/resolv.conf
     mkdir --parents /usr/local/tusk
     ln -s /vagrant /usr/local/tusk/current
 fi
 
+echo "[$sn] Adding opentusk and epel repos ..."
+
 cp /usr/local/tusk/current/install/centos-5.8/opentusk.repo /etc/yum.repos.d
 yum install --quiet -y epel-release
+
+echo "[$sn] Installing git ..."
+
 yum install --quiet -y git
+
+echo "[$sn] Installing yum packages ..."
 
 # Installing yum packages
 bash /usr/local/tusk/current/install/centos-5.8/install_yum_packages.bash
