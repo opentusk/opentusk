f15dcb9a1284eb9866beaa6774dfa0932dba26be
Pratham Joshi <pratham.joshi@tufts.edu>
2014-03-25 12:40:12 -0400

    modified course.sql file

--- upgrade/competency/course.sql
+++ upgrade/competency/course.sql
@@ -2,3 +2,7 @@
 Upgrade script for competency upgrade related to course_competencies, competency_types and the competency checklist project.
 */
 
+/*Modifications to old tables*/
+
+
+/*Creation of New tables*/
