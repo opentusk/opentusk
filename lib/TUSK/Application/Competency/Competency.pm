feaf062656eda9292564d6b3be0e1ebaac386c16
Pratham Joshi <pratham.joshi@tufts.edu>
2014-03-25 13:48:30 -0400

    grabbed Application/Competency library files from t2883-prath-competency-linking branch

---
+++ lib/TUSK/Application/Competency/Competency.pm
@@ -0,0 +1,275 @@
+# Copyright 2012 Tufts University 
+
+# Licensed under the Educational Community License, Version 1.0 (the "License"); 
+# you may not use this file except in compliance with the License. 
+# You may obtain a copy of the License at 
+
+# http://www.opensource.org/licenses/ecl1.php 
+
+# Unless required by applicable law or agreed to in writing, software 
+# distributed under the License is distributed on an "AS IS" BASIS, 
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+# See the License for the specific language governing permissions and 
+# limitations under the License.
+
+
+package TUSK::Application::Competency::Competency;
+
+use strict;
+
+use Data::Dumper; #Note: For testing purposes, remember to remove!
+
+use TUSK::Competency::Competency;
+use TUSK::Competency::ClassMeeting;
+use TUSK::Competency::Content;
+use TUSK::Competency::Course;
+use TUSK::Competency::Hierarchy;
+use TUSK::Competency::Relation;
+
+sub new{
+    my ( $class, $args ) = @_;
+
+    my $self = {
+	competency_id => $args->{competency_id},
+	title => $args->{title},
+	description => $args->{description},
+	uri => $args->{uri},
+	user_type_id => $args->{user_type_id},
+	school_id => $args->{school_id},
+	competency_level_enum_id => $args->{competency_level_enum_id},
+	version_id => $args->{version_id},
+	user => $args->{user},
+    };
+  
+    bless( $self, $class);
+
+    if ( defined( $self->{competency_id} )){
+	$self->{competency} = TUSK::Competency::Competency->lookupReturnOne( "competency_id =". $self->{competency_id} );	
+    } else{
+	$self->{competency} = TUSK::Competency::Competency->new();
+    }
+    
+    return $self;
+}
+
+#######################################################
+
+=item B<getChildren>
+
+	Return All children for a particular parent_competency, returns an arrayref.
+=cut
+
+sub getChildren {
+	my ($self, $extra_cond) = @_;
+
+	my $competency_hierarchy = TUSK::Competency::Hierarchy->lookup( "parent_competency_id=" . $self->{competency_id} );
+    
+	my @child_competencies;
+
+	foreach my $competency( @{$competency_hierarchy}){
+	    push @child_competencies, $competency->getChildCompetencyID;
+	}
+
+	return \@child_competencies;
+}
+
+
+#######################################################
+
+=item B<delete>
+    Deletes a given competency object
+=cut
+
+sub delete{
+
+    my ( $self, $extra_cond ) = @_;
+    
+    my $linked_competencies = $self->getLinked;
+
+    #if no linked child competencies, procede with deleting, otherwise not.
+    if ( (scalar(@{$linked_competencies})) == 0){
+	#delete call to database
+	my $delete_check = $self->{competency}->delete();
+
+	return $delete_check; 
+    } else { 
+	return 0;
+    }
+
+}
+
+#######################################################
+
+
+=item B<deleteAssociated>
+    Deletes a given competency object with an associated link ( i.e course_competency, session_competency or content_competency )
+=cut
+
+sub deleteAssociated{
+    
+    my ( $self, $extra_cond ) = @_;
+
+    my $competency = $self->{competency};
+
+    if ( !defined( $competency )){
+	print "Invalid entry";
+	return 0;
+    }
+
+    my $competency_level = $competency->getCompetencyLevel;
+
+    if ( $competency_level eq "course" ) {
+	my $competency_course = TUSK::Competency::Course->lookupReturnOne( "competency_id=". $competency->getPrimaryKeyID );
+	$competency_course->delete();
+    } elsif ( $competency_level eq "class_meet" ){
+	my $competency_class_meet = TUSK::Competency::ClassMeeting->lookupReturnOne( "competency_id=". $competency->getPrimaryKeyID );
+	$competency_class_meet->delete();       
+    } elsif ( $competency_level eq "content" ){
+	my $competency_content = TUSK::Competency::Content->lookupReturnOne( "competency_id=". $competency->getPrimaryKeyID );
+	$competency_content->delete();
+    } else {
+	return 0;
+    }
+    $self->delete;
+    return 1;
+}
+
+
+#######################################################
+
+=item B<update>
+    Change different values for the given competency
+=cut
+
+sub update{
+    
+    my ( $self, $extra_cond ) = @_;
+
+    my $competency = $self->{competency};
+    
+    if ( defined( $self->{title} )){
+	$competency->setTitle( $self->{title} );
+    }
+    if ( defined( $self->{description} )){
+	$competency->setDescription( $self->{description} );
+    }
+    if ( defined( $self->{uri} )){
+	$competency->setUri( $self->{uri} );
+    }
+    if ( defined( $self->{user_type_id} )){
+	$competency->setCompetencyUserTypeID(); #placeholder
+    }
+    if ( defined( $self->{school_id} )){
+	$competency->setSchoolID( $self->{school_id} );
+    }
+    if ( defined( $self->{competency_level_enum_id} )){
+	$competency->setCompetencyLevelEnumID( $self->{competency_level} );
+    }
+    if ( defined( $self->{version_id} )){
+	$competency->setVersionID( $self->{school_id} ); #version_id and school_id are same until we have multiple versions of competencies
+    }
+    $competency->save( {user => $self->{user} });
+}
+
+
+#######################################################
+
+=item B<add>
+    Adds a new competency
+=cut
+
+sub add{
+
+    my ( $self, $extra_cond ) = @_;
+
+    my $competency = $self->{competency};
+    
+    $competency->setTitle( $self->{title} );
+    $competency->setDescription( $self->{description} );
+    $competency->setUri( $self->{uri} );
+    $competency->setCompetencyUserTypeID( $self->{user_type_id} );
+    $competency->setSchoolID( $self->{school_id} );
+    $competency->setCompetencyLevelEnumID( $self->{competency_level_enum_id} );
+    $competency->setVersionID( $self->{school_id} ); 
+
+    $competency->save( { user => $self->{user} } );
+
+    return $competency->getPrimaryKeyID;
+}
+#######################################################
+
+=item B<addChild>
+    Adds a new competency as a child to the current competency
+=cut
+
+sub addChild{
+
+    my ( $self, $extra_cond ) = @_;
+    
+    print Dumper $self->{competency_id};
+
+    my $child_competency_id = $self->add;
+
+    my $hierarchy = TUSK::Competency::Hierarchy->new();
+    $hierarchy->setSchoolID( $self->{schooL_id});
+    # $hierarchy->setLineage(placeholder);
+    # $hierarhcy->setSortOrder(placeholder);
+    # $hierarchy->setDepth(placeholder);
+    $hierarchy->setParentCompetencyID( $self->{competency_id} );
+    $hierarchy->setChildCompetencyID( $child_competency_id );
+    $hierarchy->save( { user => $self->{user} } );
+}
+
+
+#######################################################
+
+
+=item B<getLinked>
+    returns the competencies that have been linked to the current competency.
+=cut
+
+sub getLinked{
+    my ( $self, $extra_cond ) = @_;
+
+    my $competency_id_1 = $self->{ competency_id };
+    
+    my $linked = TUSK::Competency::Competency->lookup( 'competency_relation.competency_id_1 =' . $competency_id_1,
+                [ 'competency_relation.competency_id_1', 'competency_relation.competency_id_2', 'competency.title', 'competency.description' ],
+                undef, undef,
+	        [ TUSK::Core::JoinObject->new('TUSK::Competency::Relation', { origkey=> 'competency_id', joinkey=> 'competency_id_2', jointype=> 'inner'})]);
+
+    return $linked;
+}
+
+#######################################################
+
+#Other functions:
+
+
+my @comp_categories;
+
+sub getCategories{
+    my ( $lib, $data ) = @_;
+    
+    getCategoriesSub( $data );
+    
+    return \@comp_categories;
+}
+
+sub getCategoriesSub{
+
+    my ( $data ) = @_;
+
+    foreach my $d (@{$data}){
+	if (($d->{'children'})){
+	    my %comp_category;
+	    $comp_category{ $d->{'id'} } = $d->{title};
+	    push @comp_categories, \%comp_category;	
+	    getCategoriesSub ( $d->{'children'} );
+	}
+    }
+    
+}
+
+
+1;
---
+++ lib/TUSK/Application/Competency/Tree.pm
@@ -0,0 +1,179 @@
+# Copyright 2012 Tufts University 
+
+# Licensed under the Educational Community License, Version 1.0 (the "License"); 
+# you may not use this file except in compliance with the License. 
+# You may obtain a copy of the License at 
+
+# http://www.opensource.org/licenses/ecl1.php 
+
+# Unless required by applicable law or agreed to in writing, software 
+# distributed under the License is distributed on an "AS IS" BASIS, 
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+# See the License for the specific language governing permissions and 
+# limitations under the License.
+
+
+package TUSK::Application::Competency::Tree;
+
+use strict;
+
+use TUSK::Application::Competency::Competency;
+use TUSK::Competency::Competency;
+use TUSK::Competency::Relation;
+
+
+sub new{
+    my ( $class, $args ) = @_;
+
+    my $self = {
+	competency_id => $args->{competency_id},
+    };
+  
+    bless( $self, $class);
+
+    $self->{competency} = TUSK::Competency::Competency->lookupReturnOne( "competency_id =". $self->{competency_id} );
+    
+    return $self;
+}
+
+#######################################################
+
+=item B<deleteFromTree>
+    #Deletes a given competency and it's child competencies (i.e. everything under it in tree)
+	
+=cut
+
+sub deleteFromTree{
+    
+    my ( $self, $extra_cond ) = @_;
+        
+    my $competency_tree = $self->getBranch;
+    deleteFromTreeHelper( $competency_tree );
+}
+
+sub deleteFromTreeHelper{
+    my ( $competency_tree ) = @_;
+       
+    foreach my $key( keys %{$competency_tree} ){
+	if (ref $competency_tree->{$key} eq 'HASH') {	    
+	    deleteFromTreeHelper( $competency_tree->{$key});
+	}
+	my $competency_args = {
+	    competency_id => $key,
+	};
+	my $competency_to_delete = TUSK::Application::Competency::Competency->new( $competency_args );
+        $competency_to_delete->delete( $key );
+	#add delete links here
+    }
+
+}
+
+
+
+#######################################################
+
+=item B<getBranch>
+   Returns entire subtree for a given parent_competency_id, returns a reference to a nested hash with indefinite dimensions.
+	
+=cut
+
+sub getBranch{
+ 
+    my ( $self, $extra_cond ) = @_;
+    
+    my %branch;
+
+    my $hash_string = '$branch->{'. $self->{competency_id}. '}';
+
+    getBranchHelper( $self->{competency_id}, $hash_string , \%branch );
+    
+    return \%branch;
+}
+
+
+sub getBranchHelper{
+     #helper function for implementing getBranch 
+
+    my ( $competency_id, $index, $branch ) = @_;
+
+    my %child_competencies;
+
+    my $competency  = {
+	competency_id => $competency_id,
+    };
+
+    my $this_competency = TUSK::Application::Competency::Competency->new( $competency );
+
+    %child_competencies = map { $_ => 0 } @{ $this_competency->getChildren };
+    my $assign = $index.' = { %child_competencies };';
+    eval( $assign );
+
+    foreach my $key(keys %child_competencies){
+	my $new_index = $index."->{".$key."}";
+	getBranchHelper( $key, $new_index, $branch );
+    }
+}
+
+
+
+######################################################
+
+
+
+sub walk{ 
+
+    my ( $lib, $competency_tree, $depth ) = @_;
+
+    $depth = $depth + 1;
+    
+    foreach my $key( keys %{$competency_tree} ){
+	print "Competency id: $key <br>";
+	if (ref $competency_tree->{$key} eq 'HASH') {
+	    print "&nbsp&nbsp&nbsp&nbsp" x $depth;
+	    walk( $lib, $competency_tree->{$key}, $depth );
+	}
+    }    
+}
+
+
+
+sub build{
+    my $competencies;
+
+    foreach( @{TUSK::Competency::Competency->new()->lookup()} ) {                                                                                                                                                                               	$competencies->{$_->getCompetencyID} = $_;                                                                                                                                                                                    
+    }   
+
+    my ( $lib, $school_id, $root_id ) = @_;
+    my $c_href;
+    my $children_storage;
+    my %p_c_pairs;
+    
+    my $cr = TUSK::Competency::Hierarchy->lookup( 'competency_hierarchy.school_id ='. $school_id, 
+						  ['depth desc', 'parent_competency_id', 'sort_order'], undef, undef,
+						  [ TUSK::Core::JoinObject->new('TUSK::Competency::Competency', { origkey=> 'child_competency_id', joinkey=> 'competency_id', jointype=> 'inner'})]);
+
+    foreach my $cr_row ( @{$cr} ) {
+	if ( defined($competencies->{$cr_row->getChildCompetencyID()})){
+	    my $child_comp = $competencies->{$cr_row->getChildCompetencyID()};
+	    if ( !defined( $c_href->{$child_comp->getCompetencyID} ) ) {
+		$c_href->{$child_comp->getCompetencyID} = { 
+		                                               id          => $child_comp->getCompetencyID, 
+							       title       => $child_comp->getTitle,
+							       description => $child_comp->getDescription,
+							       children    => $children_storage->{$child_comp->getCompetencyID},
+							   };
+			}	
+	    if ( !$p_c_pairs{$cr_row->getParentCompetencyID() . "-" . $child_comp->getCompetencyID} ) {
+		$p_c_pairs{$cr_row->getParentCompetencyID() . "-" . $child_comp->getCompetencyID} = 1;
+		push @{$children_storage->{$cr_row->getParentCompetencyID()}}, $c_href->{$child_comp->getCompetencyID} 
+	    }
+	}
+    }
+    
+    return $children_storage->{ $root_id };
+}
+
+
+
+1;
+
