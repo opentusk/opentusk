4aa850de531547686e516d6e52dd00fced4e52bd
Pratham Joshi <pratham.joshi@tufts.edu>
2014-03-25 13:56:47 -0400

    grabbed Competency ORM files Competency.pm, Course.pm, Hierarchy.pm and UserType.pm from branch t2883-prath-competency-linking

---
+++ lib/TUSK/Competency/Course.pm
@@ -0,0 +1,241 @@
+# Copyright 2013 Tufts University
+#
+# Licensed under the Educational Community License, Version 1.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.opensource.org/licenses/ecl1.php
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+package TUSK::Competency::Course;
+
+=head1 NAME
+
+B<TUSK::Competency::Course> - Class for manipulating entries in table competency_course in tusk database
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=head1 INTERFACE
+
+
+=head2 GET/SET METHODS
+
+=over 4
+
+=cut
+
+use strict;
+
+BEGIN {
+    require Exporter;
+    require TUSK::Core::SQLRow;
+
+    use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
+    
+    @ISA = qw(TUSK::Core::SQLRow Exporter);
+    @EXPORT = qw( );
+    @EXPORT_OK = qw( );
+}
+
+use vars @EXPORT_OK;
+
+# Non-exported package globals go here
+use vars ();
+
+sub new {
+    # Find out what class we are
+    my $class = shift;
+    $class = ref $class || $class;
+    # Call the super-class's constructor and give it all the values
+    my $self = $class->SUPER::new ( 
+				    _datainfo => {
+					'database' => 'tusk',
+					'tablename' => 'competency_course',
+					'usertoken' => '',
+					'database_handle' => '',
+					},
+				    _field_names => {
+					'course_competency_id' => 'pk',
+					'competency_id' => '',
+					'course_id' => '',
+					'sort_order' => '',
+				    },
+				    _attributes => {
+					save_history => 1,
+					tracking_fields => 1,	
+				    },
+				    _levels => {
+					reporting => 'cluck',
+					error => 0,
+				    },
+				    @_
+				  );
+    # Finish initialization...
+    return $self;
+}
+
+### Get/Set methods
+
+#######################################################
+
+=item B<getCompetencyID>
+
+my $string = $obj->getCompetencyID();
+
+Get the value of the competency_id field
+
+=cut
+
+sub getCompetencyID{
+    my ($self) = @_;
+    return $self->getFieldValue('competency_id');
+}
+
+#######################################################
+
+=item B<setCompetencyID>
+
+$obj->setCompetencyID($value);
+
+Set the value of the competency_id field
+
+=cut
+
+sub setCompetencyID{
+    my ($self, $value) = @_;
+    $self->setFieldValue('competency_id', $value);
+}
+
+
+
+#######################################################
+
+=item B<getCourseID>
+
+my $string = $obj->getCourseID();
+
+Get the value of the course_id field
+
+=cut
+
+sub getCourseID{
+    my ($self) = @_;
+    return $self->getFieldValue('course_id');
+}
+
+#######################################################
+
+=item B<setCourseID>
+
+$obj->setCourseID($value);
+
+Set the value of the course_id field
+
+=cut
+
+sub setCourseID{
+    my ($self, $value) = @_;
+    $self->setFieldValue('course_id', $value);
+}
+
+
+
+#######################################################
+
+=item B<getSortOrder>
+
+my $string = $obj->getSortOrder();
+
+Get the value of the sort_order field
+
+=cut
+
+sub getSortOrder{
+    my ($self) = @_;
+    return $self->getFieldValue('sort_order');
+}
+
+#######################################################
+
+=item B<setSortOrder>
+
+$obj->setSortOrder($value);
+
+Set the value of the sort_order field
+
+=cut
+
+sub setSortOrder{
+    my ($self, $value) = @_;
+    $self->setFieldValue('sort_order', $value);
+}
+
+
+#######################################################
+
+=item B<getRelationship>
+
+my $string = $obj->getRelationship();
+
+Get the value of the relationship field
+
+=cut
+
+sub getRelationship{
+    my ($self) = @_;
+    return $self->getFieldValue('relationship');
+}
+
+#######################################################
+
+=item B<setRelationship>
+
+$obj->setRelationship($value);
+
+Set the value of the relationship field
+
+=cut
+
+sub setRelationship{
+    my ($self, $value) = @_;
+    $self->setFieldValue('relationship', $value);
+}
+
+
+
+=back
+
+=cut
+
+### Other Methods
+
+=head1 BUGS
+
+None Reported.
+
+=head1 SEE ALSO
+
+B<TUSK::Core::SQLRow> - parent class
+
+=head1 AUTHOR
+
+TUSK Development Team <tuskdev@tufts.edu>
+
+=head1 COPYRIGHT
+
+Copyright (c) Tufts University Sciences Knowledgebase, 2013.
+
+=cut
+
+1;
+
---
+++ lib/TUSK/Competency/UserType.pm
@@ -0,0 +1,208 @@
+# Copyright 2013 Tufts University
+#
+# Licensed under the Educational Community License, Version 1.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.opensource.org/licenses/ecl1.php
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+package TUSK::Competency::UserType;
+
+=head1 NAME
+
+B<TUSK::Competency::UserType> - Class for manipulating entries in table competency_user_type in tusk database
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=head1 INTERFACE
+
+
+=head2 GET/SET METHODS
+
+=over 4
+
+=cut
+
+use strict;
+
+BEGIN {
+    require Exporter;
+    require TUSK::Core::SQLRow;
+
+    use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
+    
+    @ISA = qw(TUSK::Core::SQLRow Exporter);
+    @EXPORT = qw( );
+    @EXPORT_OK = qw( );
+}
+
+use vars @EXPORT_OK;
+
+# Non-exported package globals go here
+use vars ();
+
+sub new {
+    # Find out what class we are
+    my $class = shift;
+    $class = ref $class || $class;
+    # Call the super-class's constructor and give it all the values
+    my $self = $class->SUPER::new ( 
+				    _datainfo => {
+					'database' => 'tusk',
+					'tablename' => 'competency_user_type',
+					'usertoken' => '',
+					'database_handle' => '',
+					},
+				    _field_names => {
+					'competency_user_type_id' => 'pk',
+					'name' => '',
+					'competency_type_enum_id' => '',
+					'school_id' => '',
+				    },
+				    _attributes => {
+					save_history => 1,
+					tracking_fields => 1,	
+				    },
+				    _levels => {
+					reporting => 'cluck',
+					error => 0,
+				    },
+				    @_
+				  );
+    # Finish initialization...
+    return $self;
+}
+
+### Get/Set methods
+
+#######################################################
+
+=item B<getName>
+
+my $string = $obj->getName();
+
+Get the value of the name field
+
+=cut
+
+sub getName{
+    my ($self) = @_;
+    return $self->getFieldValue('name');
+}
+
+#######################################################
+
+=item B<setName>
+
+$obj->setName($value);
+
+Set the value of the name field
+
+=cut
+
+sub setName{
+    my ($self, $value) = @_;
+    $self->setFieldValue('name', $value);
+}
+
+#######################################################
+
+=item B<getSchoolID>
+
+my $string = $obj->getSchoolID();
+
+Get the value of the school_id field
+
+=cut
+
+sub getSchoolID{
+    my ($self) = @_;
+    return $self->getFieldValue('school_id');
+}
+
+#######################################################
+
+=item B<setSchoolID>
+
+$obj->setSchoolID($value);
+
+Set the value of the school_id field
+
+=cut
+
+sub setSchoolID{
+    my ($self, $value) = @_;
+    $self->setFieldValue('school_id', $value);
+}
+
+
+
+#######################################################
+
+=item B<getCompetencyTypeEnumID>
+
+my $string = $obj->getCompetencyTypeEnumID();
+
+Get the value of the competency_type_enum_id field
+
+=cut
+
+sub getCompetencyTypeEnumID{
+    my ($self) = @_;
+    return $self->getFieldValue('competency_type_enum_id');
+}
+
+#######################################################
+
+=item B<setCompetencyTypeEnumID>
+
+$obj->setCompetencyTypeEnumID($value);
+
+Set the value of the competency_type_enum_id field
+
+=cut
+
+sub setCompetencyTypeEnumID{
+    my ($self, $value) = @_;
+    $self->setFieldValue('competency_type_enum_id', $value);
+}
+
+
+
+=back
+
+=cut
+
+### Other Methods
+
+=head1 BUGS
+
+None Reported.
+
+=head1 SEE ALSO
+
+B<TUSK::Core::SQLRow> - parent class
+
+=head1 AUTHOR
+
+TUSK Development Team <tuskdev@tufts.edu>
+
+=head1 COPYRIGHT
+
+Copyright (c) Tufts University Sciences Knowledgebase, 2013.
+
+=cut
+
+1;
+
