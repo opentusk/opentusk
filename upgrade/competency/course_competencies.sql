f25de0f54769e20a7d47685ea2af3d4510c13691
Pratham Joshi <pratham.joshi@tufts.edu>
2014-04-04 19:53:10 -0400

    updated course_competencies.sql fixed ordering and syntax problems

--- upgrade/competency/course_competencies.sql
+++ upgrade/competency/course_competencies.sql
@@ -15,37 +15,6 @@ ALTER TABLE competency CONVERT TO CHARACTER SET utf8;
 ALTER TABLE competency_history CONVERT TO CHARACTER SET utf8;
 
 
-/*Add new columns to competency and comptency_history tables*/
-
-ALTER TABLE competency
-ADD COLUMN uri varchar(256) DEFAULT NULL AFTER description, 
-ADD COLUMN competency_level_enum_id int(10) unsigned DEFAULT NULL AFTER uri,
-ADD COLUMN competency_user_type_id int(10) unsigned DEFAULT NULL AFTER competency_level_enum_id
-ADD COLUMN version_id tinyint unsigned DEFAULT NULL AFTER school_id,
-ADD CONSTRAINT fk_competencytype FOREIGN KEY(competency_user_type_id) REFERENCES competency_user_type( competency_user_type_id),
-ADD CONSTRAINT fk_competencyversion FOREIGN KEY (version_id) REFERENCES competency_version (competency_version_id);
-
-ALTER TABLE competency_history
-ADD COLUMN uri varchar(256) NOT NULL AFTER description,
-ADD COLUMN competency_level_enum_id int(10) unsigned DEFAULT NULL AFTER uri,
-ADD COLUMN competency_user_type_id int(10) unsigned DEFAULT NULL AFTER competency_level_enum_id
-ADD COLUMN version_id tinyint unsigned DEFAULT NULL AFTER school_id;
-
-/* rename the competency_relationship table and the related history table to competency_hierarchy */
-
-RENAME TABLE competency_relationship TO competency_hierarchy;
-
-ALTER TABLE competency_hierarchy 
-CHANGE competency_relationship_id competency_hierarchy_id int(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-DROP column school_id;
-
-RENAME TABLE competency_relationship_history TO competency_hierarchy_history;
-
-ALTER TABLE competency_hierarchy_history 
-CHANGE competency_relationship_history_id competency_hierarchy_history_id int(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-CHANGE competency_relationship_id competency_hierarchy_id int(10) UNSIGNED,
-DROP column school_id;
-
 /*Insert user_type data and level_id data to enum_data table*/
 
 INSERT INTO enum_data 
@@ -153,6 +122,64 @@ VALUES (
 	now() 
 ); 
 
+/*user_type and related history table for linking custom user types to types in enum_data */
+
+DROP TABLE IF EXISTS competency_user_type;
+
+CREATE TABLE competency_user_type( 
+	competency_user_type_id int(10) unsigned PRIMARY KEY AUTO_INCREMENT, 
+	name varchar(24), 
+	school_id int(10) unsigned, 
+	competency_type_enum_id smallint(6),
+	created_by varchar(24) NOT NULL DEFAULT '', 
+	created_on datetime DEFAULT NULL, 
+	modified_by varchar(24) NOT NULL DEFAULT '', 
+	modified_on datetime DEFAULT NULL 
+) ENGINE=InnoDB CHARSET=UTF8; 
+
+DROP TABLE IF EXISTS competency_user_type_history;
+
+CREATE TABLE competency_user_type_history(
+	competency_user_type_history_id int(10) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+	competency_user_type_id int(10) unsigned, 
+	name varchar(24), 
+	school_id int(10) unsigned, 
+	competency_type_enum_id smallint(6),
+	created_by varchar(24) NOT NULL DEFAULT '' , 
+	created_on datetime DEFAULT NULL, 
+	modified_by varchar(24) NOT NULL DEFAULT '', 
+	modified_on datetime DEFAULT NULL,
+	history_action enum('Insert', 'Update', 'Delete')
+) ENGINE=InnoDB CHARSET=UTF8; 
+
+/*Add new columns to competency and comptency_history tables*/
+
+ALTER TABLE competency
+ADD COLUMN uri varchar(256) DEFAULT NULL AFTER description, 
+ADD COLUMN competency_level_enum_id int(10) unsigned DEFAULT NULL, 
+ADD COLUMN competency_user_type_id int(10) unsigned DEFAULT NULL,
+ADD COLUMN version_id tinyint unsigned DEFAULT NULL AFTER school_id;
+
+ALTER TABLE competency_history
+ADD COLUMN uri varchar(256) NOT NULL AFTER description,
+ADD COLUMN competency_level_enum_id int(10) unsigned DEFAULT NULL AFTER uri,
+ADD COLUMN competency_user_type_id int(10) unsigned DEFAULT NULL AFTER competency_level_enum_id,
+ADD COLUMN version_id tinyint unsigned DEFAULT NULL AFTER school_id;
+
+/* rename the competency_relationship table and the related history table to competency_hierarchy */
+
+RENAME TABLE competency_relationship TO competency_hierarchy;
+
+ALTER TABLE competency_hierarchy 
+CHANGE competency_relationship_id competency_hierarchy_id int(10) UNSIGNED,
+DROP column school_id;
+
+RENAME TABLE competency_relationship_history TO competency_hierarchy_history;
+
+ALTER TABLE competency_hierarchy_history 
+CHANGE competency_relationship_history_id competency_hierarchy_history_id int(10) UNSIGNED,
+CHANGE competency_relationship_id competency_hierarchy_id int(10) UNSIGNED,
+DROP column school_id;
 
 
 /*Creation of New tables*/
@@ -188,67 +215,3 @@ CREATE TABLE competency_course_history (
 	CONSTRAINT FOREIGN KEY (competency_id) REFERENCES competency(competency_id)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8; 
 
-
-/*user_type and related history table for linking custom user types to types in enum_data */
-
-DROP TABLE IF EXISTS competency_user_type;
-
-CREATE TABLE competency_user_type( 
-	competency_user_type_id int(10) unsigned PRIMARY KEY AUTO_INCREMENT, 
-	name varchar(24), 
-	school_id int(10) unsigned, 
-	competency_type_enum_id smallint(6),
-	created_by varchar(24) NOT NULL DEFAULT '' , 
-	created_on datetime DEFAULT NULL, 
-	modified_by varchar(24) NOT NULL DEFAULT '', 
-	modified on datetime DEFAULT NULL 
-) ENGINE=InnoDB CHARSET=UTF8; 
-
-DROP TABLE IF EXISTS competency_user_type_history;
-
-CREATE TABLE competency_user_type_history(
-	competency_user_type_history_id int(10) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-	competency_user_type_id int(10) unsigned, 
-	name varchar(24), 
-	school_id int(10) unsigned, 
-	competency_type_enum_id smallint(6),
-	created_by varchar(24) NOT NULL DEFAULT '' , 
-	created_on datetime DEFAULT NULL, 
-	modified_by varchar(24) NOT NULL DEFAULT '', 
-	modified_on datetime DEFAULT NULL,
-	history_action enum('Insert', 'Update', 'Delete')
-) ENGINE=InnoDB CHARSET=UTF8; 
-
-/*Insert default data to competency_user_Type*/
-INSERT INTO competency_user_type VALUE(
-	0,
-	"Competency",
-	<competency_type_enum_id>,
-	1,
-	"script",
-	now(),
-	"script",
-	now()
-);
-
-INSERT INTO competency_user_type VALUE(
-	0,
-	"Competency Category",
-	<competency_type_enum_id>,
-	1,
-	"script",
-	now(),
-	"script",
-	now()
-);
-
-INSERT INTO competency_user_type VALUE(
-	0,
-	"Supporting Information",
-	<competency_type_enum_id>,
-	1,
-	"script",
-	now(),
-	"script",
-	now()
-);
