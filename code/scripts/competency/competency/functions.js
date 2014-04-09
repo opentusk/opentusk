dccd0a23bf50e55ea809cc880a3de80c31a30c06
Pratham Joshi <pratham.joshi@tufts.edu>
2014-04-09 14:57:54 -0400

    added comments to buildCompetencyChecklistTree function in competency/competency/functions.js script

--- code/scripts/competency/competency/functions.js
+++ code/scripts/competency/competency/functions.js
@@ -50,15 +50,48 @@ function currentCompLabel( current_competency ){
 
 //Functions related to competency checklist division/popup
 
-function buildCompetencyChecklistTree( dialog_name, school_id, course_id, input_type, children, display_type ){
+function buildCompetencyChecklistTree( dialog_name, school_name, course_id, input_type, children, display_type ){
+
+/*
+Uses the "<competencyRoot>/tmpl/display" page and given parameters to build a competency checklist tree and displays it in the given division.
+The competency_id for the selected competency from the checklist is stored in the javascript variable selected_competency_id after a user clicks on "select" 
+and can then be used accordingly.
+
+	Requires:
+			jQuery libraries and jQuery UI libraries.
+						- "jquery/jquery.min.js"
+						- "jquery/jquery-ui.min.js"
+						- "jquery/jquery.ui.widget.min.js"
+						- "jquery/plugin/interface/interface.js"
+
+	Parameters:
+			dialog_name = HTML 'id' for the div used to display the dialog_box. Div must be present on caller page.
+			school_name = School that the course belongs to Eg. Medical, Dental, etc.
+			course_id   = HSDB45 course id for the course that we want to create the checklist for.
+			input_type  = Determines whether the checklist items compromise of:
+					"radio" = radio buttons ( able to select one )
+					"checkbox" = checkboxes ( able to select multiple )
+			children    = Determines whether the child competencies of the top level competencies are selectable or not:
+					"on" = selectable
+					"off" = not selectable
+			display_type = Determines whether the checklist division is displayed as:
+					"inline" = inline to other HTML elements in the page
+					"dialog" = displayed as a popup dialog box
+	Example Usage: 
+			<div id = "test_dialog"</div>
+			<input type="button" value="Display Checklist" onclick="buildCompetencyChecklistTree('test_dialog', 'Dental', 1251, 'radio', 'off', 'inline');
+			(The above example has a button with value "Display Checklist" which when clicked displays a competency checklist tree for course 1251 of the Dental 
+			School, consisting of radio buttons with child competencies unselectable inline on the "test_dialog" division.)
+ */
+
 	if( display_type == "inline" ){
-		$( "#" + dialog_name ).load( competencyRoot + "tmpl/display/school/Medical" , {school_id: school_id, course: course_id, input_type: input_type, children: children });
+		$( "#" + dialog_name ).load( competencyRoot + "tmpl/display/course/" + school_name + "/" + course_id, {school_name: school_name, course: course_id, input_type: input_type, children: children });
 	} else if( display_type == "dialog" ){
 		$( "#" + dialog_name).css({
 			'background' : 'white',
 			'border' : '1px solid'
 		});
-		$( "#" + dialog_name ).load( competencyRoot + "tmpl/display/school/Medical" , {school_id: school_id, course: course_id, input_type: input_type, children: children }).dialog( { dialogClass: 'checklist_dialog_class', title: ' ' });
+		$( "#" + dialog_name ).load( competencyRoot + "tmpl/display/school/" + school_name + "/" + course_id, {school_name: school_name, course: course_id, input_type: input_type, children: children }).dialog( { dialogClass: 'checklist_dialog_class', title: ' ' });
 
 		$( "#" + dialog_name ).css({
 			"width": 600,
