cec886f7f87d3df70488c56a50118ce2ec29d961
Pratham Joshi <pratham.joshi@tufts.edu>
2014-04-27 15:21:09 -0400

    modified description in add_verion.pl

--- upgrade/competency/add_version.pl
+++ upgrade/competency/add_version.pl
@@ -28,13 +28,12 @@ foreach my $school( @$schools){
     my $school_id = $school->getPrimaryKeyID();
     my $school_name = $school->getSchoolName();
     my $title = $school_name . " version 1";
-    my $description = "first version";
+    my $description = "Version 1 Competencies for ". $school_name;
     my $sql = qq( INSERT INTO tusk.competency_version VALUES ( $school_id, $school_id, '$title', '$description', now(), 'migration', now() ));
     my $sth = $dbh->prepare( $sql );
     $sth->execute();
     $sth->finish;
     print $school->getPrimaryKeyID()."\n";    
-
 }
 
 
