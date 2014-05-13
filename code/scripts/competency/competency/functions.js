8e26f7561679173cd163eb564949cfdf987398e7
Pratham Joshi <pratham.joshi@tufts.edu>
2014-05-13 14:58:44 -0400

    fixed append for not just schedule, global

--- code/scripts/competency/competency/functions.js
+++ code/scripts/competency/competency/functions.js
@@ -113,12 +113,12 @@ function closeLinkWindow() {
 function appendNewLinkedCompetencies(competency_id) {
 	$.each( to_update_array, function( index, value ) {
 		var competency_desc = value.replace(/&nbsp;/g, '');	
-		$('#competency_container #class_meeting_competencies').find('li[id^='+ competency_id + '] .col1').find('.competency_popup_container a').append("<i>New " + (index+1) + ": </i>" + competency_desc.substring(0,50) +  "<br>");
-		$('#competency_container #class_meeting_competencies').find('li[id^=' + competency_id + '] .col1').find('.competency_popup_content').append("<b><i>New " + (index+1) + ": </i></b>" + competency_desc + "<br>");	
+		$('#competency_container').find('li[id^='+ competency_id + '] .col1').find('.competency_popup_container a').append("<i>New " + (index+1) + ": </i>" + competency_desc.substring(0,50) +  "<br>");
+		$('#competency_container').find('li[id^=' + competency_id + '] .col1').find('.competency_popup_content').append("<b><i>New " + (index+1) + ": </i></b>" + competency_desc + "<br>");	
 	});
 	$.each( to_update_array_remove, function( index, value ) {
 		var competency_desc = value.replace(/&nbsp;/g, '');	
-		var to_delete = $('#competency_container #class_meeting_competencies').find('li[id^='+ competency_id + '] .col1').find('.competency_popup_container a');
+		var to_delete = $('#competency_container').find('li[id^='+ competency_id + '] .col1').find('.competency_popup_container a');
 		comp_match_pattern = new RegExp(value.replace(/&nbsp;/g, '').substring(0,50), 'g');
 		var temp_html = to_delete.html();
 		temp_html = temp_html.replace(comp_match_pattern, "<i>deleted</i>");
