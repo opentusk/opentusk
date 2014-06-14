05b237e6ea89bc6fc6671109ed9d3116e418015b
Pratham Joshi <pratham.joshi@tufts.edu>
2014-06-14 13:46:24 -0400

    fixed problem withe course_competency_tables.sql from release 4.2

--- upgrade/tusk-4_2_0/course_competency_tables.sql
+++ upgrade/tusk-4_2_0/course_competency_tables.sql
@@ -105,8 +105,8 @@ RENAME TABLE competency_relationship_history TO competency_hierarchy_history;
 ALTER TABLE competency_hierarchy_history 
 DROP COLUMN created_by,
 DROP COLUMN created_on,
-CHANGE competency_relationship_history_id competency_hierarchy_history_id int(10) UNSIGNED,
-CHANGE competency_relationship_id competency_hierarchy_id int(10) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT;
+CHANGE competency_relationship_history_id competency_hierarchy_history_id int(10) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CHANGE competency_relationship_id competency_hierarchy_id int(10) unsigned NOT NULL; 
 
 /*Creation of New tables*/
 
