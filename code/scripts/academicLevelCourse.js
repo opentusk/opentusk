cee05650321fd4b188f14b7009202ca754b694fa
Pratham Joshi <pratham.joshi@tufts.edu>
2014-06-20 13:49:40 -0400

    update for link academic level and course, added update file for updating relations

--- code/scripts/academicLevelCourse.js
+++ code/scripts/academicLevelCourse.js
@@ -7,7 +7,14 @@ function showAcadLevels (this_button) {
 	$(this_button).hide();
 	$(this_button).parent().find(".acad_level_dropdown").dropdownchecklist({
 			firstItemChecksAll : true,
-			explicitClose: 'Done'
+			explicitClose: 'Save',
+			onComplete: function(selector) {
+				for (i=0; i < selector.options.length; i++){
+					if (selector.options[i].selected){
+						console.log(selector.options[i].value);
+					}
+				}
+			}
 	});
 	$(this_button).parent().find(".ui-dropdownchecklist").trigger("click");
 }
