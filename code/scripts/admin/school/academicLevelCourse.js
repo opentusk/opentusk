6db504c0b0b0b17824721c2edcbc448febf9f39a
Pratham Joshi <pratham.joshi@tufts.edu>
2014-06-23 11:11:47 -0400

    academic level updates pre-ajaxcall

--- code/scripts/admin/school/academicLevelCourse.js
+++ code/scripts/admin/school/academicLevelCourse.js
@@ -11,7 +11,8 @@ function showAcadLevels (this_button) {
 			width : '300px',
 			explicitClose: 'Save&nbsp',
 			onComplete: function(selector) {
-				var option_count = 0;				
+				var option_count = 0;	
+				var to_update_array = [];			
 				for (i=0; i < selector.options.length; i++) {
 					if (selector.options[i].selected) {
 						var current_label = $(current_academic_level).html();
@@ -33,12 +34,28 @@ function showAcadLevels (this_button) {
 							}							
 						}
 						option_count++;
+						to_update_array.push(selector.options[i].value);
 					}					
 				}
 				if (option_count == 0) {
 					$(current_academic_level).html("Uncategorized ");
 				}
+				$(to_update_array).each( function(index) {
+					console.log(to_update_array[index]);
+				});
+				course_link = $(this_button).parent().parent().find('a').first();
+				course_id = course_link.attr('href').split('/')[5];				
 				
+				jQuery.each(to_add_array, function(i, val) {
+					$.ajax({
+						type: "POST",
+						url: "/tusk/admin/school/academicLevel/update",
+						data: {academic_level_id: val, course_id: course_id}
+					}).done(function() {
+						alert("Academic Levels updated successfully.");
+				  });
+				});
+
 				$(current_academic_level).show();
 				$(this_button).parent().find(".acad_level_dropdown").dropdownchecklist("destroy");
 				$(this_button).show();
