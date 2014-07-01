1ff02a0c26c02b573d19c7accae4c9ed2de95a4d
Pratham Joshi <pratham.joshi@tufts.edu>
2014-07-01 08:48:01 -0400

    added pm library object files for tables year and time_period_year

---
+++ lib/TUSK/AcademicLevel/Year.pm
@@ -0,0 +1,177 @@
+# Copyright 2013 Tufts University
+#
+# Licensed under the Educational Community License, Version 1.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.opensource.org/licenses/ecl1.php
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+package TUSK::AcademicLevel::Year;
+
+=head1 NAME
+
+B<TUSK::AcademicLevel::Year> - Class for manipulating entries in table year in tusk database
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=head1 INTERFACE
+
+
+=head2 GET/SET METHODS
+
+=over 4
+
+=cut
+
+use strict;
+
+BEGIN {
+    require Exporter;
+    require TUSK::Core::SQLRow;
+
+    use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
+    
+    @ISA = qw(TUSK::Core::SQLRow Exporter);
+    @EXPORT = qw( );
+    @EXPORT_OK = qw( );
+}
+
+use vars @EXPORT_OK;
+
+# Non-exported package globals go here
+use vars ();
+
+sub new {
+    # Find out what class we are
+    my $class = shift;
+    $class = ref $class || $class;
+    # Call the super-class's constructor and give it all the values
+    my $self = $class->SUPER::new ( 
+				    _datainfo => {
+					'database' => 'tusk',
+					'tablename' => 'year',
+					'usertoken' => '',
+					'database_handle' => '',
+					},
+				    _field_names => {
+					'year_id' => 'pk',
+					'title' => '',
+					'description' => '',
+				    },
+				    _attributes => {
+					save_history => 0,
+					tracking_fields => 1,	
+					no_created => 0,
+				    },
+				    _levels => {
+					reporting => 'cluck',
+					error => 0,
+				    },
+				    @_
+				  );
+    # Finish initialization...
+    return $self;
+}
+
+### Get/Set methods
+
+#######################################################
+
+=item B<getTitle>
+
+my $string = $obj->getTitle();
+
+Get the value of the title field
+
+=cut
+
+sub getTitle{
+    my ($self) = @_;
+    return $self->getFieldValue('title');
+}
+
+#######################################################
+
+=item B<setTitle>
+
+$obj->setTitle($value);
+
+Set the value of the title field
+
+=cut
+
+sub setTitle{
+    my ($self, $value) = @_;
+    $self->setFieldValue('title', $value);
+}
+
+
+#######################################################
+
+=item B<getDescription>
+
+my $string = $obj->getDescription();
+
+Get the value of the description field
+
+=cut
+
+sub getDescription{
+    my ($self) = @_;
+    return $self->getFieldValue('description');
+}
+
+#######################################################
+
+=item B<setDescription>
+
+$obj->setDescription($value);
+
+Set the value of the description field
+
+=cut
+
+sub setDescription{
+    my ($self, $value) = @_;
+    $self->setFieldValue('description', $value);
+}
+
+
+
+=back
+
+=cut
+
+### Other Methods
+
+=head1 BUGS
+
+None Reported.
+
+=head1 SEE ALSO
+
+B<TUSK::Core::SQLRow> - parent class
+
+=head1 AUTHOR
+
+TUSK Development Team <tuskdev@tufts.edu>
+
+=head1 COPYRIGHT
+
+Copyright (c) Tufts University Sciences Knowledgebase, 2013.
+
+=cut
+
+1;
+
