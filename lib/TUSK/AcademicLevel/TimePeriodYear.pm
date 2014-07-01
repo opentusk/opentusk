5f89afba67d8aedc0ed9fa635ad739ff7c882fd4
Pratham Joshi <pratham.joshi@tufts.edu>
2014-07-01 08:50:54 -0400

    set no_created to true in timeperiodyear.pm and year.pm

--- lib/TUSK/AcademicLevel/TimePeriodYear.pm
+++ lib/TUSK/AcademicLevel/TimePeriodYear.pm
@@ -73,7 +73,7 @@ sub new {
 				    _attributes => {
 					save_history => 0,
 					tracking_fields => 1,	
-					no_created => 0,
+					no_created => 1,
 				    },
 				    _levels => {
 					reporting => 'cluck',
