6adee70c7d231be85e72da46f58f8cadd3cf2ffa
Pratham Joshi <pratham.joshi@tufts.edu>
2014-07-17 14:46:49 -0400

    added drop foreign key for history table on academic_levels_table_update.sql upgrade script

--- upgrade/tusk-4_3_2/academic_levels_table_update.sql
+++ upgrade/tusk-4_3_2/academic_levels_table_update.sql
@@ -1,5 +1,8 @@
 /*Add sort_order column to academic_level and academic_level_history tables*/
 
+ALTER TABLE academic_level_course_history
+DROP FOREIGN KEY academic_level_course_history_ibfk_1;
+
 ALTER TABLE academic_level
 ADD COLUMN sort_order tinyint(3) unsigned NOT NULL DEFAULT '0' AFTER school_id;
 
