db0b79e78d15df878f12b026ea91f710918dabd9
Erden <erden.oktay@tufts.edu>
2014-07-29 16:27:07 -0400

    Configured CKEditor to render HTML5 source code properly [TUS-3031]

--- code/scripts/ckeditor/config.js
+++ code/scripts/ckeditor/config.js
@@ -27,7 +27,7 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Se the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
-	
+	config.allowedContent = true;
 	// Adjust the skin 
 	config.skin = 'kama';
 	
