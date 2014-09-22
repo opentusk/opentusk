7fea88809cad34571a7d3ebcb5d6670d6d05c886
Isarin Sathitruangsak <isarin.sathitruangsak@tufts.edu>
2014-09-22 08:13:27 -0400

    added class meeting group ie they are the same class_meetings

--- upgrade/ci-xml/tables.sql
+++ upgrade/ci-xml/tables.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS `class_meeting_assessment_purpose` (
   `modified_by` varchar(24) NOT NULL DEFAULT '',
   `modified_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`class_meeting_assessment_purpose_id`),
-  UNIQUE KEY `class_meeting_id` (`class_meeting_id`,`assessment_purpose_enum_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+  UNIQUE KEY `class_meeting_id` (`class_meeting_id`,`assessment_purpose_enum_id`, `school_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `class_meeting_assessment_purpose_history` (
   `class_meeting_assessment_purpose_history_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -19,7 +19,32 @@ CREATE TABLE IF NOT EXISTS `class_meeting_assessment_purpose_history` (
   `modified_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `history_action` enum('Insert','Update','Delete') DEFAULT NULL,
   PRIMARY KEY (`class_meeting_assessment_purpose_history_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `class_meeting_group` (
+  `class_meeting_group_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `class_meeting_id1` int(10) unsigned NOT NULL,
+  `class_meeting_id2` int(10) unsigned NOT NULL,
+  `school_id` int(10) unsigned NOT NULL,
+  `modified_by` varchar(24) NOT NULL DEFAULT '',
+  `modified_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`class_meeting_group_id`),
+  UNIQUE KEY `class_meeting_id` (`class_meeting_id1`, `class_meeting_id2`, `school_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `class_meeting_group_history` (
+  `class_meeting_group_history_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `class_meeting_group_id` int(10) unsigned NOT NULL,
+  `class_meeting_id1` int(10) unsigned NOT NULL,
+  `class_meeting_id2` int(10) unsigned NOT NULL,
+  `school_id` int(10) unsigned NOT NULL,
+  `modified_by` varchar(24) NOT NULL DEFAULT '',
+  `modified_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `history_action` enum('Insert','Update','Delete') DEFAULT NULL,
+  PRIMARY KEY (`class_meeting_group_history_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 alter table class_meeting_type
 add curriculum_method_enum_id int(10) unsigned NULL after label,
