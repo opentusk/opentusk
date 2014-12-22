b58dedf4f7f0ef2def859a3e7ed2cdc1f8c74755
Pratham Joshi <pratham.joshi@tufts.edu>
2014-12-22 15:06:29 -0500

    update to final grade export WIP

--- code/scripts/grade/export.js
+++ code/scripts/grade/export.js
@@ -19,19 +19,58 @@ $(document).ready(function() {
 			});
 		});
 
+		var trial;
+
 		$.each(grades, function (uid, grade) {
 			var temp_hash = {};
 			temp_hash.uid = uid;
 			temp_hash.grade = grade;
 			temp_hash.name = students_processed[uid]; 
 			json.push(temp_hash);			
+			trial = JSON.stringify(temp_hash);
 		});
 
+		console.log(trial);
+
+		console.log(json);
+
+		/*
 		$.post( "/tusk/admin/grade/export", function(data){
 		});
+		*/
 	});
 
 });
+
+var input;
+var url;
+var lastSaved;
+ 
+function doJSON(validJSONInput) {
+  // get input JSON, try to parse it
+  var newInput = validJSONInput;
+  if (newInput == input) return;
+ 
+  input = newInput;
+  if (!input) {
+    return;
+  }
+  var json = jsonFrom(input);
+  doCSV(json);
+  return true;
+}
+ 
+function doCSV(json) {
+  var inArray = arrayFrom(json);
+  var outArray = [];
+  for (var row in inArray)
+      outArray[outArray.length] = parse_object(inArray[row]);
+ 
+  var csv = $.csv.fromObjects(outArray);
+  var uri = "data:text/csv;charset=utf-8," + encodeURIComponent(csv);
+  window.location.href = uri;
+}
+
 
 function generate() {
 	var form=document.createElement('form');
@@ -40,8 +79,11 @@ function generate() {
 	form.style.display = 'hidden';
 	var data = document.createElement("input");
 	var json_text = JSON.stringify(json);
-	data.setAttribute("name", json_text);
+	console.log(json_text);
+	data.setAttribute("name", "json");
+	data.setAttribute("value", json_text);
 	form.appendChild(data);
+	console.log(data);
 	document.body.appendChild(form);
 	form.submit();
 }
