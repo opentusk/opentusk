ff9cd5a78f04b70d9b0dd24fe3cda6d4ee32903f
Pratham Joshi <pratham.joshi@tufts.edu>
2014-12-23 10:36:00 -0500

    grade export for final grades now working requires clean-up

--- code/scripts/grade/export.js
+++ code/scripts/grade/export.js
@@ -1,4 +1,6 @@
 var json =[];
+var time_period;
+var course_id;
 
 $(document).ready(function() {
 	$("#export").click(function() {
@@ -11,9 +13,11 @@ $(document).ready(function() {
 			});
 		});
 
-		$(grades).each(function (index, object) {
+		$(grades).each(function (index, object) {			
 			$.each(object, function(index_2, object_2) {
+				time_period = index_2;
 				$.each(object_2, function(index_3, object_3) {
+					course_id = index_3;
 					grades = object_3;
 				});
 			});
@@ -77,13 +81,33 @@ function generate() {
 	form.setAttribute('method', 'post');
 	form.setAttribute('action', '/tusk/admin/grade/export');
 	form.style.display = 'hidden';
+
 	var data = document.createElement("input");
+	var data_2 = document.createElement("input");
+	var data_3 = document.createElement("input");
+	var data_4 = document.createElement("input");
+
 	var json_text = JSON.stringify(json);
 	console.log(json_text);
 	data.setAttribute("name", "json");
 	data.setAttribute("value", json_text);
 	form.appendChild(data);
-	console.log(data);
+	
+	data_2.setAttribute("name", "period");
+	data_2.setAttribute("value", time_period);
+	form.appendChild(data_2);
+	
+	data_3.setAttribute("name", "course");
+	data_3.setAttribute("value", course_id);
+	form.appendChild(data_3);
+
+	var currentURL = window.location.pathname;
+
+	data_4.setAttribute("name", "school");
+	currentURL = currentURL.split('/');
+	data_4.setAttribute("value", currentURL[currentURL.length - 1]);
+	form.appendChild(data_4);
+
 	document.body.appendChild(form);
 	form.submit();
 }
