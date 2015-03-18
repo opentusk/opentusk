32b347b6a9fdab1bac4cd9c51992a1ee421fb2f7
Pratham Joshi <pratham.joshi@tufts.edu>
2015-03-18 16:39:38 -0400

    academic_year update for clinical placement WIP

--- code/scripts/clinicalschedule.js
+++ code/scripts/clinicalschedule.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 	$("#current_academic_year").change(function () {
 		var year = this.value;
 		url = window.location.href;
-		url += "?year=" + year;
+		url += "?year=" + year;	
+		alert(url);
 		window.location.href = url;
-		location.reload();
 	});
 });
