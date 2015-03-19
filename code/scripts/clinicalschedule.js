64dce4a8c626e9ca29a2a397f8b84a14dbe934d5
Pratham Joshi <pratham.joshi@tufts.edu>
2015-03-19 10:22:32 -0400

    academic year update WIP

--- code/scripts/clinicalschedule.js
+++ code/scripts/clinicalschedule.js
@@ -2,14 +2,24 @@ $(document).ready(function() {
 	$("#current_academic_year").change(function () {
 		var year = this.value;
 		var url = window.location.href;
+	
+		jQuery('<form>', {
+			'action' : url,
+			'target' : '_top'
+		}).append(jQuery('<input>', {
+			'name': 'academic_year',
+			'value' : year,
+			'type' : 'hidden'
+		})).appendTo('body')
+		.submit();
 
 		alert(this.value);
-
+		/*
 		$.post(url, 
 			{academic_year : this.value},
 			reloadpage
 		);
-		/*
+
 		$.ajax({
 			type: "POST",
 			url: url,
