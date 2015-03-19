b64ffdbca3f7f844e59678ffd868b0de822b37fb
Pratham Joshi <pratham.joshi@tufts.edu>
2015-03-19 09:51:21 -0400

    clinical placement academic year ajax test

--- code/scripts/clinicalschedule.js
+++ code/scripts/clinicalschedule.js
@@ -2,11 +2,7 @@ $(document).ready(function() {
 	$("#current_academic_year").change(function () {
 		var year = this.value;
 		var url = window.location.href;
-		url += "?year=";
-		url += this.value;
 		alert(this.value);
-		//alert(url);
-		//window.location.href = url;
 		$.ajax({
 			type: "POST",
 			url: url,
