d3ac06d84a801ac26a2476b1b6c2501074a06cae
Pratham Joshi <pratham.joshi@tufts.edu>
2015-03-19 10:05:57 -0400

    clinical schedule update wip

--- code/scripts/clinicalschedule.js
+++ code/scripts/clinicalschedule.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
 	$("#current_academic_year").change(function () {
 		var year = this.value;
 		var url = window.location.href;
+
 		alert(this.value);
+
 		$.post(url, 
 			{academic_year : this.value},
 			reloadpage
