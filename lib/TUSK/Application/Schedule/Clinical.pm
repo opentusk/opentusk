54062cad9a3d7c69995e2bc607d5c1695ff102c2
Erden <erden.oktay@tufts.edu>
2015-03-23 11:33:31 -0400

    Ordered the query result set by time period start date [TUS-3185]

--- lib/TUSK/Application/Schedule/Clinical.pm
+++ lib/TUSK/Application/Schedule/Clinical.pm
@@ -41,7 +41,7 @@ sub getScheduleCourses{
 	my @siteNames = ();
 
 	my $scheduleCourses = TUSK::Academic::LevelClinicalSchedule->new();
-	my $dB = $scheduleCourses->databaseSelect(
+	my $sth = $scheduleCourses->databaseSelect(
 		"SELECT t6.title, t7.period, t7.start_date, t7.end_date, t8.site_name, t6.course_id
 		FROM " . $scheduleCourses->getDatabase() . ".academic_level_clinical_schedule AS t1 
 		INNER JOIN tusk.academic_level AS t2
@@ -58,19 +58,20 @@ sub getScheduleCourses{
 		ON t7.time_period_id = t5.time_period_id
 		LEFT JOIN " . $self->{school_db} . ".teaching_site AS t8
 		ON t8.teaching_site_id = t5.teaching_site_id
-		WHERE (t5.child_user_id = '$self->{user_id}' AND t1.school_id = '$self->{school_id}')"
+		WHERE (t5.child_user_id = '$self->{user_id}' AND t1.school_id = '$self->{school_id}')
+		ORDER BY t7.start_date"
 	);
 
-	while (my ($dBTitle, $dBPeriod, $dBStartDate, $dBEndDate, $dBSiteName, $dBCourseId) = $dB->fetchrow_array()) {
-		push @courseIds, $dBCourseId;
-		push @courseTitles, $dBTitle;
-		push @timePeriods, $dBPeriod;
-		push @startDates, $dBStartDate;
-		push @endDates, $dBEndDate;
-		push @siteNames, $dBSiteName;
+	while (my ($title, $period, $startDate, $endDate, $siteName, $courseId) = $sth->fetchrow_array()) {
+		push @courseIds, $courseId;
+		push @courseTitles, $title;
+		push @timePeriods, $period;
+		push @startDates, $startDate;
+		push @endDates, $endDate;
+		push @siteNames, $siteName;
 	}
 
-	$dB->finish();
+	$sth->finish();
 
 	return (\@courseIds, \@courseTitles, \@timePeriods, \@startDates, \@endDates, \@siteNames);
 }
