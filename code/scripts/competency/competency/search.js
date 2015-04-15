868d05c505599f535e56773b3603c3b77c1cde4b
Pratham Joshi <pratham.joshi@tufts.edu>
2015-04-15 13:28:12 -0400

    added javascript file for competency search

---
+++ code/scripts/competency/competency/search.js
@@ -0,0 +1,22 @@
+$(function() {
+	$("#domain_dropdown").val(0);
+	$("#competency_dropdown").val(0);
+});
+
+function loadTopLevelCompetencies(domain) {
+	$.ajax({				
+			type: "POST",
+			url: "/tusk/competency/search/ajaxTopLevelCompetencies",
+			data: {competency_id: domain.value},
+						
+		}).success(function(data) {
+			$("#competency_dropdown").empty();
+			var blankCompetency = $('<option value="">' + "-- Select a Competency --" + '</option>');
+			$("#competency_dropdown").append(blankCompetency);
+			data = $.parseJSON(data);
+			$.each(data, function(index, competency_object) {
+				var newCompetency = $('<option value="' + competency_object.competency_id + '">' + competency_object.title + '</option>');
+				$("#competency_dropdown").append(newCompetency);
+			});
+		});
+}
