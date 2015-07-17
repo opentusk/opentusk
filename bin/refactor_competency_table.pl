de5827b78346e0892ad980b0a4a1b23ddbca6034
Pratham Joshi <pratham.joshi@tufts.edu>
2015-07-17 15:49:47 -0400

    completed content, course and description moving on competency refactor script

--- bin/refactor_competency_table.pl
+++ bin/refactor_competency_table.pl
@@ -36,14 +36,61 @@ sub refactorContentCourseSession {
     use Data::Dumper; #remove this
     
     my $competency_levels = grabLevels();
-
     print Dumper $competency_levels;
 
     print "Moving Course Competencies...\n";
+    my $course_competencies = TUSK::Competency::Competency->lookup("competency_level_enum_id = $competency_levels->{course}");
+
+    my $temp_counter = 0;
+
+    foreach my $course_competency (@{$course_competencies}) {
+	$temp_counter++;
+	if ($temp_counter > 5) {
+	    last;
+	}
+
+	$course_competency->setTitle($course_competency->getDescription());
+	$course_competency->save({user => "script"});
+	print $course_competency->getTitle();
+	print "\n\n";
+    }
 
     print "Moving Content Competencies...\n";
+
+    my $content_competencies = TUSK::Competency::Competency->lookup("competency_level_enum_id = $competency_levels->{content}");
+
+    $temp_counter = 0;
+
+    foreach my $content_competency (@{$content_competencies}) {
+	$temp_counter++;
+	if ($temp_counter > 5) {
+	    last;
+	}
+
+	$content_competency->setTitle($content_competency->getDescription());
+	$content_competency->save({user => "script"});
+	print $content_competency->getTitle();
+	print "\n\n";
+    }
+
     
     print "Moving Class Meeting Competencies...\n";
+
+    my $class_meeting_competencies = TUSK::Competency::Competency->lookup("competency_level_enum_id = $competency_levels->{class_meet}");
+
+    $temp_counter = 0;
+
+    foreach my $class_meeting_competency (@{$class_meeting_competencies}) {
+	$temp_counter++;
+	if ($temp_counter > 5) {
+	    last;
+	}
+
+	$class_meeting_competency->setTitle($class_meeting_competency->getDescription());
+	$class_meeting_competency->save({user => "script"});
+	print $class_meeting_competency->getTitle();
+	print "\n\n";
+    }
 }
 
 sub refactorSchool {
@@ -54,7 +101,7 @@ sub grabLevels {
     my $competency_levels = TUSK::Enum::Data->lookup("namespace = 'competency.level_id'");
     my %competency_levels_hash;
     foreach my $competency_level (@{$competency_levels}) {
-	$competency_levels_hash{$competency_level->getPrimaryKeyID()} = $competency_level->getShortName();
+	$competency_levels_hash{$competency_level->getShortName()} = $competency_level->getPrimaryKeyID();
     }
     return \%competency_levels_hash;
 }
