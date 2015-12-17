ed5c2ff5d8b71676b0511d359f494c6a3c97e4ed
Elad Tsur <elad.tsur@tufts.edu>
2015-12-17 14:09:44 -0500

    Make MySQL authentication compatible with MariaDB [TUS-3379]

--- lib/HSDB45/Authentication/MySQL.pm
+++ lib/HSDB45/Authentication/MySQL.pm
@@ -1,15 +1,15 @@
-# Copyright 2012 Tufts University 
+# Copyright 2012 Tufts University
 #
-# Licensed under the Educational Community License, Version 1.0 (the "License"); 
-# you may not use this file except in compliance with the License. 
-# You may obtain a copy of the License at 
+# Licensed under the Educational Community License, Version 1.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# http://www.opensource.org/licenses/ecl1.php 
+# http://www.opensource.org/licenses/ecl1.php
 #
-# Unless required by applicable law or agreed to in writing, software 
-# distributed under the License is distributed on an "AS IS" BASIS, 
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-# See the License for the specific language governing permissions and 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
 # limitations under the License.
 
 
@@ -29,36 +29,28 @@ sub verify_password {
     my $user = shift;
     my $inpw = shift;
     # Check that we got an input value
-    return unless $inpw;
-    my ($db_pw, $old_sqlpw, $sqlpw, $cryptpw) = ('', '', '');
+    return unless ($inpw);
+    my ($dbpw, $sqlpw, $old_sqlpw) = ('', '', '');
     # Protect us from us
     eval {
-	# Make a connection
-	my $dbh = HSDB4::Constants::def_db_handle();
-	# Quote the incoming PW
-        my ($quser, $qpw) = 
-	    map { $dbh->quote ($_) } ($user->primary_key, $inpw);
-        my $sql = qq[SELECT password, old_password($qpw), password($qpw),
-		     ENCRYPT($qpw, LEFT(password,2))
-		     FROM user WHERE user_id=$quser];
-        ($db_pw, $old_sqlpw, $sqlpw, $cryptpw) = $dbh->selectrow_array ($sql);
+        # Make a connection
+        my $dbh = HSDB4::Constants::def_db_handle();
+        # Quote the incoming PW
+        my ($quser, $qpw) = map { $dbh->quote($_) } ($user->primary_key(), $inpw);
+        my $sql = "SELECT password, PASSWORD($qpw), OLD_PASSWORD($qpw) FROM user WHERE user_id = $quser";
+        ($dbpw, $sqlpw, $old_sqlpw) = $dbh->selectrow_array($sql);
     };
 
     ## first check to see if we got a password
-    return unless $sqlpw && $old_sqlpw && $db_pw && $cryptpw;
+    return unless ($dbpw && $sqlpw && $old_sqlpw);
 
-    if ($db_pw eq $sqlpw) {
+    if ($dbpw eq $sqlpw) {
        ## matches on new password, return success
-       $user->field_value ('password', $db_pw);
-       return 1;       
-    } elsif ($db_pw eq $old_sqlpw) {
-       ## matches on old password, update to new and return success
-       $user->field_value ('password', $sqlpw);
-       $user->save($TUSK::Constants::DatabaseUsers{ContentManager}->{writeusername},
-	           $TUSK::Constants::DatabaseUsers{ContentManager}->{writepassword});
        return 1;
-    } elsif ($db_pw eq $cryptpw) {
-       ## the password matches the temp password, return success
+    } elsif ($dbpw eq $old_sqlpw) {
+       ## matches on old password, update to new and return success
+       $user->field_value('password', $sqlpw);
+       $user->save();
        return 1;
     }
 
@@ -79,5 +71,3 @@ sub verify {
 }
 
 1;
-
-
