977e57b6c079a452ee282c7b7cf4e1d429ba591a
Erden <erden.oktay@tufts.edu>
2016-02-18 16:52:15 -0500

    Included updates for `modified_by` and `modified_on` columns [TUS-3446]

--- upgrade/tusk-4_7_6/updateClinicalAdminRoleDesc.sql
+++ upgrade/tusk-4_7_6/updateClinicalAdminRoleDesc.sql
@@ -10,14 +10,19 @@ SELECT @x_role_id := role_id, @x_function_id := function_id
         AND t2.function_desc = 'View schedules');
 
 UPDATE tusk.permission_role
-SET role_desc = 'Ability to view and edit schedules for clinical studies'
+SET role_desc = 'Ability to view and edit schedules for clinical studies',
+	modified_by = 'sqlscript',
+	modified_on = NOW()
 WHERE (role_token = 'clinical_director'
     AND feature_type_id = 2
     AND role_id = @x_role_id
     AND role_desc = 'Ability to view schedules for clinical studies');
 
 UPDATE tusk.permission_function
-SET function_token = 'view_edit_schedules', function_desc = 'View and edit schedules'
+SET function_token = 'view_edit_schedules', 
+	function_desc = 'View and edit schedules',
+	modified_by = 'sqlscript',
+	modified_on = NOW()
 WHERE (function_token = 'view_schedules' AND function_desc = 'View schedules' AND function_id = @x_function_id);
 //
 delimiter ;
--- upgrade/tusk-4_8_4/siteDirectorRoleFunction.sql
+++ upgrade/tusk-4_8_4/siteDirectorRoleFunction.sql
@@ -1,7 +1,7 @@
 delimiter //
 SET @x_role_id = 0;
 SET @x_function_id = 0;
-INSERT INTO tusk.permission_function (function_token, function_desc) VALUES ('view_site_grades', 'View site grades');
+INSERT INTO tusk.permission_function (function_token, function_desc, created_by, created_on, modified_by, modified_on) VALUES ('view_site_grades', 'View site grades', 'sqlscript', NOW(), 'sqlscript', NOW());
 SELECT @x_role_id := role_id, @x_function_id := function_id 
 	FROM tusk.permission_role AS t1 
 		INNER JOIN tusk.permission_function AS t2 
@@ -9,6 +9,6 @@ SELECT @x_role_id := role_id, @x_function_id := function_id
 		AND t1.role_desc = 'Site Director' 
 		AND t2.function_token = 'view_site_grades' 
 		AND t2.function_desc = 'View site grades');
-INSERT INTO tusk.permission_role_function(role_id, function_id) values(@x_role_id, @x_function_id);
+INSERT INTO tusk.permission_role_function(role_id, function_id, created_by, created_on, modified_by, modified_on) values(@x_role_id, @x_function_id, 'sqlscript', NOW(), 'sqlscript', NOW());
 // 
 delimiter ;
