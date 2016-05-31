95ce4030d537a41831abe2a31a91201ff396e0cc
Isarin S <isathi01@tufts.edu>
2016-05-30 22:24:16 -0400

    refactored to allow student to add a new assessor in lieu of using 'other'. this will add user to user table and doing all the appropriate linkages

--- code/scripts/competency/checklist/emailfaculty.js
+++ code/scripts/competency/checklist/emailfaculty.js
@@ -1,4 +1,4 @@
-// Copyright 2012 Tufts University 
+// Copyright 2014 Tufts University 
 //
 // Licensed under the Educational Community License, Version 1.0 (the "License"); 
 // you may not use this file except in compliance with the License. 
@@ -12,36 +12,64 @@
 // See the License for the specific language governing permissions and 
 // limitations under the License.
 
-$(function() {
-	$('#to').on('change', function() {
-		if (this.value == 'other') {
-			$('#otherbox').show('slow');
-		} else {
-			$('#otherbox').hide('slow');
-			$('#other_to').val("");
-		}
-	});
 
-	$("#emailfaculty").validate({
-		rules: {
-			other_to: {
-				email: true,
-				required: {
-					depends: function() {
-						return ($('#to').val() == 'other') ? true : false;
-					}
-				}
-			},
-		},
-		messages: {
-			other_to: "Please enter a valid email address",
-		}
-	});
-});
+function showOther(pathIds) {
 
+    var theBox = $("#otherbox").dialog({
+        autoOpen: false,
+        position: [ 'top', 100 ],
+        title: 'Add Assessor',
+        draggable: false,
+        width : 450,
+        height : 180, 
+        resizable : false,
+        modal : true,
+        closeOnEscape: false,
+        open: function(event, ui) {
+            $(".ui-dialog-titlebar-close", ui.dialog | ui).hide();
+        	$('#addassessor').validate({
+        		rules: {
+		        	email: { required : true },
+        			firstname: { required : true },
+        			lastname: { required : true }
+    		}
+	        });
+        },
+        buttons: {
+            Add: function() {
+                 $.ajax({
+                    url     : '/competency/checklist/student/addassessor/course/' + pathIds,
+                    type    : 'POST',
+                    data    : $("#addassessor").serialize(),
+                    success : function(resp) {
+                        if ($('#addassessor').valid()) {
+                            $('#addassessor').hide();
+                            $('#results').html(resp);
+                            $('.ui-button:contains(Add)').hide();
+                            var html = $.parseHTML(resp);
+                            $('#to').append('<option value="' + $(html).filter('#new_user_token').text() + '" selected="selected">' + $(html).filter('#new_user_name').text() + '</option>');
+                        }
+                    },
+                    error   : function(resp){
+                        $('#result').html(resp);
+                    }
+                 });
+            },
+            Close: function() {
+                $(this).dialog( "close" );
+                if ($('#addassessor').is(':visible') === false) {
+                    $('#addassessor').show();
+                    $('#results').hide();
+                    $('.ui-button:contains(Add)').show();
+                }
+            }
+        }
+    });
 
-
-
+    $("#otherbox").load('/competency/checklist/student/addassessorform/course/' + pathIds, function() {
+            theBox.dialog("open");
+    });
+}
 
 
 
