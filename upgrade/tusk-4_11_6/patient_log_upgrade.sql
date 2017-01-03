0b1c96ac1e18debd56ca2db1ce9a5c417fe770ea
Pratham Joshi <pratham.joshi@tufts.edu>
2017-01-03 13:52:31 -0500

    added upgrade instructions for TUS-3651

---
+++ upgrade/tusk-4_11_6/patient_log_upgrade.sql
@@ -0,0 +1,6 @@
+INSERT INTO form_builder_field_type VALUES (0, "ConfidentialPatientIdentifier", "Confidential Patient Identifier", "script", now(), "script", now());
+
+SELECT * FROM form_builder_field WHERE field_name = "Confidential Identifier";
+
+UPDATE form_builder_field SET field_type_id = (SELECT field_type_id FROM form_builder_field_type where token = "ConfidentialPatientIdentifier") WHERE field_name = "Confidential Identifier";
+
