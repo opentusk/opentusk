fac84b93beebff16d5ed6bd6cfb418b576ad8480
Isarin S <isathi01@tufts.edu>
2017-08-06 12:01:58 -0400

    fixed utf8 decoding that worked with older perl. the bug was it outputs array ref as string

--- lib/MasonX/Plugin/UTF8.pm
+++ lib/MasonX/Plugin/UTF8.pm
@@ -1,15 +1,15 @@
-# Copyright 2012 Tufts University 
+# Copyright 2012 Tufts University
 #
-# Licensed under the Educational Community License, Version 1.0 (the "License"); 
-# you may not use this file except in compliance with the License. 
-# You may obtain a copy of the License at 
+# Licensed under the Educational Community License, Version 1.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# http://www.opensource.org/licenses/ecl1.php 
+# http://www.opensource.org/licenses/ecl1.php
 #
-# Unless required by applicable law or agreed to in writing, software 
-# distributed under the License is distributed on an "AS IS" BASIS, 
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-# See the License for the specific language governing permissions and 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
 # limitations under the License.
 
 
@@ -21,8 +21,14 @@ sub start_request_hook {
     my ( $self, $context ) = @_;
     my $args_ref = $context->args();
     foreach my $arg ( @{$args_ref} ) {
-        my $result = utf8::decode($arg);
-        die unless $result;
+         if (ref($arg) eq "ARRAY" ) {
+            my $max = scalar(@{$arg}) - 1;
+            for (my $i = 0; $i<=$max; $i++){
+                utf8::is_utf8($arg->[$i]) || utf8::decode($arg->[$i]);
+            }
+        } else {
+            utf8::is_utf8($arg) || utf8::decode($arg);
+        }
     }
     return;
 }
