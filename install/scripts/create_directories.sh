71fad345b2272e2bff6beba276a8a642d50e8aeb
Isarin S <isathi01@tufts.edu>
2017-12-20 01:43:02 -0500

    added sudo

--- install/scripts/create_directories.sh
+++ install/scripts/create_directories.sh
@@ -1,16 +1,15 @@
 #! /bin/bash
 
-mkdir -p  /data/{eval_data,mysql}
-mkdir -p  /data/ppt/{native-archive,native,error,processed,temp}
-mkdir -p  /data/{.ppt,forum_data,temp}
-mkdir -p  /data/TUSKdoc/{native-archive,native,processed}
-mkdir -p  /data/streaming/{flashpix,smil,code,video}
-mkdir -p  /data/html/{shockwave,HSDB-doc,ramfiles,smil,web,downloadable_file,fop-pdf,fop-xml}
-mkdir -p  /data/html/web-auth/pdf
-mkdir -p  /data/html/images
-mkdir -p  /data/html/slide/{orig,small,icon,thumb,medium,large,xlarge}
-mkdir -p  /data/html/slide/overlay/{orig,small,medium,large,xlarge}
-mkdir -p  /usr/local/tusk/{conf,ssl_certificate}
-mkdir -p /var/log/tusk
-chown tusk:tusk /var/log/tusk
-ln -s /var/log/tusk /usr/local/tusk/current/logs
+sudo mkdir -p  /data/{eval_data,mysql}
+sudo mkdir -p  /data/ppt/{native-archive,native,error,processed,temp}
+sudo mkdir -p  /data/{.ppt,forum_data,temp}
+sudo mkdir -p  /data/TUSKdoc/{native-archive,native,processed}
+sudo mkdir -p  /data/streaming/{flashpix,smil,code,video}
+sudo mkdir -p  /data/html/{shockwave,HSDB-doc,ramfiles,smil,web,downloadable_file,fop-pdf,fop-xml}
+sudo mkdir -p  /data/html/web-auth/pdf
+sudo mkdir -p  /data/html/images
+sudo mkdir -p  /data/html/slide/{orig,small,icon,thumb,medium,large,xlarge}
+sudo mkdir -p  /data/html/slide/overlay/{orig,small,medium,large,xlarge}
+sudo mkdir -p  /usr/local/tusk/{conf,ssl_certificate}
+sudo mkdir -p /var/log/tusk
+sudo chown apache:apache /var/log/tusk
