e64313793067083d367555352838385de9161c06
Isarin S <isathi01@tufts.edu>
2017-12-27 22:56:33 -0500

    Apache2::Connection changed its object method method from remote_ip to client_ip

--- lib/TUSK/Apache2/ProxyRemoteAddr.pm
+++ lib/TUSK/Apache2/ProxyRemoteAddr.pm
@@ -1,15 +1,15 @@
-# Copyright 2012 Tufts University 
+# Copyright 2012 Tufts University
 #
-# Licensed under the Educational Community License, Version 1.0 (the "License"); 
-# you may not use this file except in compliance with the License. 
-# You may obtain a copy of the License at 
+# Licensed under the Educational Community License, Version 1.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# http://www.opensource.org/licenses/ecl1.php 
+# http://www.opensource.org/licenses/ecl1.php
 #
-# Unless required by applicable law or agreed to in writing, software 
-# distributed under the License is distributed on an "AS IS" BASIS, 
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-# See the License for the specific language governing permissions and 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
 # limitations under the License.
 
 package TUSK::Apache2::ProxyRemoteAddr;
@@ -26,7 +26,7 @@ sub handler {
     my $r = shift;
     # unset the X-Forwarded header and set the connection remote_ip
     if ( defined $r->headers_in->{'X-Forwarded-For'} ) {
-        $r->connection->remote_ip( $r->headers_in->{'X-Forwarded-For'} );
+        $r->connection->client_ip( $r->headers_in->{'X-Forwarded-For'} );
         $r->headers_in->unset('X-Forwarded-For');
     }
 
