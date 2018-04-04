781dcb14b2c88faf7d109c4c7387818fda08d809
Isarin S <isathi01@tufts.edu>
2018-04-04 08:48:43 -0400

    Cleaned up old scripts

--- install/centos-5.8/create_system_accounts.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/usr/sbin/groupadd -g 1100 tusk &> /dev/null
-/usr/sbin/useradd -c 'Tusk' -u 1100 -g tusk -d /usr/local/tusk tusk \
-    &> /dev/null
-/usr/sbin/usermod -a -G tusk apache &> /dev/null
-chmod 755 /usr/local/tusk
